84f5747087e472ab2bf1f4f2553327c80e289d0a
Shane Geary <stg4462@comcast.net>
2020-02-01 15:22:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,11 @@ class Song
   @@count = 0 
   @@artists = [] 
   @@genres = [] 
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    
+  end 
 end 
